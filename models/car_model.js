d729d2c6ac37aad80d5f40fdbf73d86988169fb4
unknown <salla.pr@hotmail.com>
2024-04-20 22:19:07 +0300

    REST API valmis

--- models/car_model.js
+++ models/car_model.js
@@ -17,7 +17,7 @@ const car = {
   delete: function(id, callback) {
     return db.query('delete from car where id_car=?', [id], callback);
   },
-  update: function(id, book, callback) {
+  update: function(id, car, callback) {
     return db.query(
       'update car set brand=?, model=? where id_car=?',
       [car.brand, car.model, id],
